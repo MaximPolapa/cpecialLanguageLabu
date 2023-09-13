f8fea8f91845c8271e54e890e33a9cf8fbf99be2
MaximPolapa <90515650+MaximPolapa@users.noreply.github.com>
2023-09-14 00:59:26 +0300

    second

--- Laba1/main.py
+++ Laba1/main.py
@@ -1,5 +1,18 @@
 import math
 
+class Memory:
+    def __init__(self):
+        self.memory = []
+
+    def store(self, expression, result):
+        self.memory.append((expression, result))
+
+    def recall(self):
+        return self.memory
+
+    def clear(self):
+        self.memory = []
+
 def calculate(num1, num2, operator):
     if operator == '+':
         return num1 + num2
@@ -8,7 +21,7 @@ def calculate(num1, num2, operator):
     elif operator == '*':
         return num1 * num2
     elif operator == '/':
-        if num2 != 0:  
+        if num2 != 0:
             return num1 / num2
         else:
             return "Помилка: ділення на нуль"
@@ -27,19 +40,32 @@ def calculate(num1, num2, operator):
     else:
         return "Помилка: недійсний оператор"
 
+memory = Memory()
+
 while True:
-    num1 = float(input("Введіть перше число: "))
-    num2 = float(input("Введіть друге число: ")) 
+    try:
+        num1 = float(input("Введіть перше число: "))
+        num2 = float(input("Введіть друге число: "))
+    except ValueError:
+        print("Помилка: Ви вказали не число. Будь ласка, введіть число 1 або 2.")
+        continue
+
     while True:
         operator = input("Введіть оператор (+, -, *, /, ^, √, %): ")
         if operator in ['+', '-', '*', '/', '^', '√', '%']:
             result = calculate(num1, num2, operator)
             print(f"Результат: {result}")
+
+            memory.store(f"{num1} {operator} {num2}", result)
             break
         else:
             print("Помилка: недійсний оператор. Будь ласка, введіть один із +, -, *, /, ^, √, %.")
 
-   
+    memory_values = memory.recall()
+    print(f"Значення в пам'яті:")
+    for expression, result in memory_values:
+        print(f"{expression} = {result}")
+
     repeat = input("Бажаєте виконати ще одне обчислення? (так/ні): ")
     if repeat.lower() != 'так':
         break
